d5781141ab9fdd3f38b88da3271f5df0d7745e5e
tao.zhou <dr_eam@foxmail.com>
2022-03-31 19:35:43 +0800

    Dockerfile

--- src/ConfigProvider.php
+++ src/ConfigProvider.php
@@ -195,6 +195,12 @@ class ConfigProvider
                     'destination' => BASE_PATH . '/README.md',
                 ],
                 [
+                    'id' => 'Dockerfile',
+                    'description' => 'replace Dockerfile',
+                    'source' => __DIR__ . '/../publish/Dockerfile',
+                    'destination' => BASE_PATH . '/Dockerfile',
+                ],
+                [
                     'id' => 'create_model_has_attachments_table',
                     'description' => 'create_model_has_attachments_table',
                     'source' => __DIR__ . '/../publish/1111_01_11_111111_create_model_has_attachments_table.php',
