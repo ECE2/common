8b7c2be5cd19d0af4d8fc84e2e1acdfddff569b8
tao.zhou <dr_eam@foxmail.com>
2022-08-10 17:44:25 +0800

    新增 查询用户所属部门数据 rpc; 新增根据条件查询部门数据 rpc

--- src/JsonRpc/Contract/SystemDeptServiceInterface.php
+++ src/JsonRpc/Contract/SystemDeptServiceInterface.php
@@ -6,24 +6,15 @@ interface SystemDeptServiceInterface
 {
     public function getByIds(array $ids);
 
-    /**
-     * 根据组集集合模糊查询获取部门数据.
-     * @param $deptId
-     * @return mixed
-     */
+    /** 根据条件获取部门数据 */
+    public function getByConditions($params = []);
+
+    /** 根据组集集合模糊查询获取部门数据 */
     public function getByLevelFuzzy($deptId);
 
-    /**
-     * 部门下的所有人 ID (包含部门下的部门).
-     * @param $id
-     * @return mixed
-     */
+    /** 部门下的所有人 ID (包含部门下的部门) */
     public function everyoneIds($id);
 
-    /**
-     * 部门的顶级部门.
-     * @param $deptId
-     * @return mixed
-     */
+    /** 部门的顶级部门 */
     public function topLevelDept($deptId);
 }
--- src/JsonRpc/Contract/SystemUserServiceInterface.php
+++ src/JsonRpc/Contract/SystemUserServiceInterface.php
@@ -6,15 +6,24 @@ interface SystemUserServiceInterface
 {
     public function getByIds(array $ids);
 
+    /** 根据用户 ID 获取用户所属部门 */
+    public function getUserDept(int $uid);
+
+    /** 根据 token 获取用户信息 */
     public function getInfoByJwtToken(string $jwtToken, string $scene = 'admin');
 
+    /** 是否为管理员角色 */
     public function isAdminRole();
 
+    /** 获取用户信息 */
     public function getInfo($uid);
 
+    /** 用户角色 */
     public function getRoles($uid);
 
+    /** 根据部门 ID 数组获取用户数据 */
     public function getByDeptIds(array $deptIds);
 
+    /** 发送消息 */
     public function sendMessageToUser($uid, $message, $type);
 }
