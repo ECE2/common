1c6d558979706bbb19ea7cc4f0afd7ff0cd61d5c
tao.zhou <dr_eam@foxmail.com>
2022-08-12 00:10:35 +0800

    处理 rpcHasOne n+1 问题

--- src/Model/Rpc/Relations/HasOneForRpc.php
+++ src/Model/Rpc/Relations/HasOneForRpc.php
@@ -11,22 +11,29 @@ use Hyperf\Utils\Arr;
 class HasOneForRpc extends HasOne
 {
     /**
-     * 重写 model 获取关联数据.
+     * 重写 relation 获取关联数据.
      * @throws \Psr\Container\ContainerExceptionInterface
      * @throws \Psr\Container\NotFoundExceptionInterface
      * @return \Hyperf\Database\Model\Collection
      */
     public function getEager()
     {
+        $builder = $this->applyScopes();
+
         /** @var Base $related */
         $related = $this->getRelated();
-
         // 整合关联条件数据, id 关联
-        return $related->newCollection($related::get(($this->getBaseQuery()->wheres[0] ?? [])['values'] ?? []));
+        $idsCondition = (array) Arr::get($this->getBaseQuery()->wheres, '0.values', []);
+        // 处理 N + 1 问题
+        if (count($models = $related::get($idsCondition)) > 0) {
+            $models = $builder->eagerLoadRelations($models);
+        }
+
+        return $builder->getModel()->newCollection($models);
     }
 
     /**
-     * 重写 model getAttribute 获取.
+     * 重写 relation getAttribute 获取.
      * @throws \Psr\Container\ContainerExceptionInterface
      * @throws \Psr\Container\NotFoundExceptionInterface
      * @return null|Base|\Hyperf\Database\Model\Builder|\Hyperf\Database\Model\Model|object
@@ -38,7 +45,7 @@ class HasOneForRpc extends HasOne
 
         // 获取远端数据
         /** @var Base $result */
-        $result = Arr::first($related::get((array) (($this->getBaseQuery()->wheres[0] ?? [])['value'] ?? [])), default: collect([]));
+        $result = Arr::first($related::get((array) Arr::get($this->getBaseQuery()->wheres, '0.values', [])), default: collect([]));
 
         return $related->setRawAttributes($result?->toArray(), true);
     }
