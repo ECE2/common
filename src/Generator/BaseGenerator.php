ae75c09a9c4d3f5182c0d7cd5118fdca6597bb8d
ECE2 <dr_eam@foxmail.com>
2023-05-21 10:15:48 +0800

    代码生成器 afk

---
+++ src/Generator/BaseGenerator.php
@@ -0,0 +1,54 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ece2\Common\Generator;
+
+use Psr\Container\ContainerInterface;
+
+abstract class BaseGenerator
+{
+    protected string $stubDir;
+
+    protected string $namespace;
+
+    public const NO  = 1;
+
+    public const YES = 2;
+
+    public function __construct(protected ContainerInterface $container)
+    {
+        $this->setStubDir(__DIR__ . '/Stubs/');
+    }
+
+    public function getStubDir(): string
+    {
+        return $this->stubDir;
+    }
+
+    public function setStubDir(string $stubDir)
+    {
+        $this->stubDir = $stubDir;
+    }
+
+    /**
+     * @return string
+     */
+    public function getNamespace(): string
+    {
+        return $this->namespace;
+    }
+
+    /**
+     * @param mixed $namespace
+     */
+    public function setNamespace(string $namespace): void
+    {
+        $this->namespace = $namespace;
+    }
+
+    public function replace(): self
+    {
+        return $this;
+    }
+}
---
+++ src/Interfaces/CodeGenerator.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ece2\Common\Interfaces;
+
+interface CodeGenerator
+{
+    public function generator();
+
+    public function preview();
+}
