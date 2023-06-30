a12a7c527aef69eb8d027f08e4208995fb894002
ECE2 <dr_eam@foxmail.com>
2023-06-30 14:05:01 +0800

    恢复注释

--- src/Model/Rpc/Model/SystemUser.php
+++ src/Model/Rpc/Model/SystemUser.php
@@ -84,29 +84,29 @@ class SystemUser extends Base
         return true;
     }
 
-//    /**
-//     * 非公司人员.
-//     */
-//    public function nonCompanyUser(): bool
-//    {
-//        return in_array($this->user_type, [self::TYPE_USER_SUPER_ADMIN, self::TYPE_USER_OPERATION, self::TYPE_USER_GOVERNMENT_REGULATION]);
-//    }
-//
-//    /**
-//     * 公司人员.
-//     */
-//    public function companyUser(): bool
-//    {
-//        return in_array($this->user_type, [self::TYPE_USER_BUSINESS_GENERALLY, self::TYPE_USER_BUSINESS_MANAGEMENT]);
-//    }
-//
-//    /**
-//     * 是否为超级管理员（创始人），用户禁用对创始人没用.
-//     */
-//    public function isSuperAdmin(): bool
-//    {
-//        return $this->user_type === self::TYPE_USER_SUPER_ADMIN;
-//    }
+    /**
+     * 非公司人员.
+     */
+    public function nonCompanyUser(): bool
+    {
+        return in_array($this->user_type, [self::TYPE_USER_SUPER_ADMIN, self::TYPE_USER_OPERATION, self::TYPE_USER_GOVERNMENT_REGULATION]);
+    }
+
+    /**
+     * 公司人员.
+     */
+    public function companyUser(): bool
+    {
+        return in_array($this->user_type, [self::TYPE_USER_BUSINESS_GENERALLY, self::TYPE_USER_BUSINESS_MANAGEMENT]);
+    }
+
+    /**
+     * 是否为超级管理员（创始人），用户禁用对创始人没用.
+     */
+    public function isSuperAdmin(): bool
+    {
+        return $this->user_type === self::TYPE_USER_SUPER_ADMIN;
+    }
 
     /**
      * 用户详情.
