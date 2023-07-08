8c1a70723140eaf7c88125816eaec77f6c59a169
ECE2 <dr_eam@foxmail.com>
2023-07-08 13:29:19 +0800

    has attachments 注释

--- src/Model/Traits/HasAttachments.php
+++ src/Model/Traits/HasAttachments.php
@@ -5,8 +5,12 @@ declare(strict_types=1);
 namespace Ece2\Common\Model\Traits;
 
 use Ece2\Common\Model\Rpc\Model\Attachment;
+use Hyperf\Database\Model\Collection;
 use Hyperf\Database\Model\Relations\MorphToMany;
 
+/**
+ * @property Collection<Attachment> $attachments
+ */
 trait HasAttachments
 {
     use HasRelationshipsForRpc;
