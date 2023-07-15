35af7714b3acd51d6c6e485d3e80cd97e6caf38c
tom <tom@gmail.com>
2023-07-15 11:24:30 +0800

    提供更新用户数据的rpc方法;

--- src/Abstracts/AbstractJsonRpcService.php
+++ src/Abstracts/AbstractJsonRpcService.php
@@ -7,9 +7,9 @@ use Ece2\Common\Traits\JsonRpcTrait;
 abstract class AbstractJsonRpcService
 {
     /**
-     * @throws \Psr\Container\ContainerExceptionInterface
-     * @throws \Psr\Container\NotFoundExceptionInterface
      * @return array
+     * @throws \Psr\Container\NotFoundExceptionInterface
+     * @throws \Psr\Container\ContainerExceptionInterface
      */
     public function success(mixed $data = [], ?string $message = null, int $code = 200)
     {
@@ -22,9 +22,9 @@ abstract class AbstractJsonRpcService
     }
 
     /**
-     * @throws \Psr\Container\ContainerExceptionInterface
-     * @throws \Psr\Container\NotFoundExceptionInterface
      * @return array
+     * @throws \Psr\Container\NotFoundExceptionInterface
+     * @throws \Psr\Container\ContainerExceptionInterface
      */
     public function error(string $message = '', int $code = 500, array $data = [])
     {
@@ -36,13 +36,42 @@ abstract class AbstractJsonRpcService
         ];
     }
 
+    /**
+     * 通过ID获取数据.
+     * @param array $ids
+     * @return array
+     * @throws \Psr\Container\ContainerExceptionInterface
+     * @throws \Psr\Container\NotFoundExceptionInterface
+     */
     public function getByIds(array $ids)
     {
         return $this->success($this->model::query()->find($ids)->toArray());
     }
 
+    /**
+     * 通过查询条件获取数据.
+     * @param string $sql
+     * @param array $bindings
+     * @param string $boolean
+     * @return array
+     * @throws \Psr\Container\ContainerExceptionInterface
+     * @throws \Psr\Container\NotFoundExceptionInterface
+     */
     public function getByWhereRaw($sql, $bindings = [], $boolean = 'and')
     {
         return $this->success($this->model::query()->whereRaw($sql, $bindings, $boolean)->get()->toArray());
+    }
+
+    /**
+     * 更新一条数据.
+     * @param int $id
+     * @param array $data
+     * @return array
+     * @throws \Psr\Container\ContainerExceptionInterface
+     * @throws \Psr\Container\NotFoundExceptionInterface
+     */
+    public function update($id, $data)
+    {
+        return $this->success($this->model::query()->findOrFail($id)?->update($data));
     }
 }
--- src/JsonRpc/Contract/SystemMemberServiceInterface.php
+++ src/JsonRpc/Contract/SystemMemberServiceInterface.php
@@ -8,4 +8,12 @@ interface SystemMemberServiceInterface
 
     /** 根据 token 获取用户信息 */
     public function getInfoByJwtToken(string $jwtToken, string $scene = 'member');
+
+    /**
+     * 根据ID更新数据.
+     * @param int $id
+     * @param array $data
+     * @return mixed
+     */
+    public function update(int $id, array $data);
 }
