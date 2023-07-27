527abbcd76034d148fbbad2feaf945fa40256bb2
tom <tom@gmail.com>
2023-07-27 14:11:16 +0800

    增加根据组获取配置的rpc接口;

--- src/JsonRpc/Contract/SettingConfigServiceInterface.php
+++ src/JsonRpc/Contract/SettingConfigServiceInterface.php
@@ -4,5 +4,17 @@ namespace Ece2\Common\JsonRpc\Contract;
 
 interface SettingConfigServiceInterface
 {
+    /**
+     * 根据 key 获取配置.
+     * @param string $key
+     * @return array
+     */
     public function getConfigByKey(string $key): array;
+
+    /**
+     * 根据组获取配置.
+     * @param array $codes
+     * @return array
+     */
+    public function getConfigByGroup(array $codes): array;
 }
