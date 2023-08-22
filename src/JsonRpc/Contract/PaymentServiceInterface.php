81d53d2181818212aea91bfaa8bf1754b4e46e69
ECE2 <dr_eam@foxmail.com>
2023-08-23 00:41:31 +0800

    挪退款

--- src/JsonRpc/Contract/PaymentServiceInterface.php
+++ src/JsonRpc/Contract/PaymentServiceInterface.php
@@ -8,6 +8,15 @@ interface PaymentServiceInterface
      * 支付单.
      * @param string $tradeNo 支付台单号
      * @param array $orderInfo 订单信息
+     * @return array
      */
-    public function order(string $tradeNo, array $orderInfo);
+    public function order(string $tradeNo, array $orderInfo): array;
+
+    /**
+     * 退款.
+     * @param string $tradeNo 支付台单号
+     * @param int $refundAmount 退款金额
+     * @return array
+     */
+    public function refund(string $tradeNo, int $refundAmount): array;
 }
