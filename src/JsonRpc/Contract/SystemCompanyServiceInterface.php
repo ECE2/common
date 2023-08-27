0b6ef04d58453756be2055367dc84948c6e15630
ECE2 <dr_eam@foxmail.com>
2023-08-27 15:55:55 +0800

    根据省市查企业 rpc

--- src/JsonRpc/Contract/SystemCompanyServiceInterface.php
+++ src/JsonRpc/Contract/SystemCompanyServiceInterface.php
@@ -7,4 +7,12 @@ interface SystemCompanyServiceInterface
     public function getByIds(array $ids);
 
     public function getCompanyBySubDomain(string $subDomain);
+
+    /**
+     * 根据省市名获取企业 (优先市, 如果没有就查省, 再没有就查默认企业).
+     * @param $provinceName
+     * @param $cityName
+     * @return mixed
+     */
+    public function getCompanyByProvinceCityName($provinceName, $cityName);
 }
